1f0d824d585af3f015bb9f0c792aa3a8116a1fc9
Arya Gharib <aryakhan81@icloud.com>
2018-07-06 09:46:15 -0700

    Initial commit

--- CurrencyCalculator/ViewController.swift
+++ CurrencyCalculator/ViewController.swift
@@ -9,7 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Control vars
+    var dollarIsInControl = true
+    var whichCurrency = 0
+    
+    //Calculate function
+    func calculate(){
+        switch <#value#> {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
+        }
+        if dollarIsInControl{
+            let dollars: Float = (NumberFormatter().number(from: dollarAmount.text!)?.floatValue)!
+            let pounds: Float = dollars * 0.7554
+            let text = String(format: "%.2f", arguments: [pounds])
+            poundAmount.text = text
+        }
+        else {
+            let pounds: Float = (NumberFormatter().number(from: poundAmount.text!)?.floatValue)!
+            let dollars: Float = pounds * 1.3237
+            let text = String(format: "%.2f", arguments: [dollars])
+            dollarAmount.text = text
+        }
+    }
 
+    //Dollars
+    @IBOutlet weak var dollarView: UIView!
+    @IBOutlet weak var dollarLabel: UILabel!
+    @IBOutlet weak var dollarAmount: UITextField!
+    
+    //Pounds
+    @IBOutlet weak var poundView: UIView!
+    @IBOutlet weak var poundLabel: UILabel!
+    @IBOutlet weak var poundAmount: UITextField!
+    
+    //Chooser
+    @IBOutlet weak var currencyCollector: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +57,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Convert dollar amount
+    @IBAction func convertDollar(_ sender: UIButton) {
+        self.dollarIsInControl = true
+        self.calculate()
+    }
+    
+    //Convert other currency
+    @IBAction func convertPound(_ sender: UIButton) {
+        self.dollarIsInControl = false
+        self.calculate()
+    }
+    
 
 }
 
